46732304a9ab5d1a4a19d593d39c48dce45323da
Ahnm1 <hansson.alexander@live.se>
2023-02-02 14:40:11 +0100

    Now a class

--- EkonomiSCB/Plot.py
+++ EkonomiSCB/Plot.py
@@ -1,25 +1,53 @@
+from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
-import numpy as np
 import os
 
-CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-def get_data():
-	in_file = CURR_DIR_PATH + '/merge/' + 'clean_konkurser_1994-2022_from_api.csv'
-	df = pd.read_csv(in_file)
-	return df
+class PlotXY:
+    def __init__(self):
+        self.CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-print(len(get_data()))
 
-def plot_data(df):
-	fig = px.scatter(
-		df, 
-		x = 'date',
-		y = 'layoffs',
-		# color = 'date'
-	)
-	return fig.show()
+    def get_data(self, file):
+        in_file = self.CURR_DIR_PATH + file
+        df      = pd.read_csv(in_file)
+        return df
 
-plot_data(get_data())
+
+    def set_max_min_color(self, df):
+        return ['green' if x == min(df) else 'red' if x == max(df) else 'black' for x in df]
+
+
+    def plot_data(self, df, x, y, title):
+        # fig = go.Figure()
+
+        fig = px.scatter(
+            df, 
+            x = x,
+            y = y,
+            color = self.set_max_min_color(df['layoffs']),
+            title = title
+            )
+        return fig
+
+
+if __name__ == '__main__':
+    app = Dash(__name__)
+
+    def set_layout(fig):
+
+        app.layout   = html.Div(
+            children = [
+
+                html.H1(children = 'Economy'),
+                dcc.Graph(figure = fig)
+                ]
+            )
+    plotting = PlotXY()
+    data     = plotting.get_data('/merge/' + 'clean_konkurser_1994-2022_from_api.csv')
+    title    = 'Individuals layed off because of bankruptcy'
+    set_layout(plotting.plot_data(data, 'date', 'layoffs', title))
+    
+    app.run_server(debug=True, use_reloader=True)
 
