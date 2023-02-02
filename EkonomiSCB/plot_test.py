9eda4e1213e23e26bbb2f6c0fa9a53737ddee4cf
Ahnm1 <hansson.alexander@live.se>
2023-02-02 20:39:52 +0100

    structure of those files, again

--- EkonomiSCB/plot_test.py
+++ EkonomiSCB/plot_test.py
@@ -13,7 +13,7 @@ def set_layout(fig):
             ]
         )
 plotting = PlotXY()
-data     = plotting.get_data('/merge/' + 'clean_konkurser_1994-2022_from_api.csv')
+data     = plotting.get_data('/Konkurser/merge/' + 'clean_konkurser_1994-2022_from_api.csv')
 set_layout(plotting.plot_data(data, 'date', 'layoffs', 'Individuals layed off because of bankruptcy'))
 
 app.run_server(debug=True, use_reloader=True)
